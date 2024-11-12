577b4351ac336097c019dbdfda5280e1ba2bdd98
Yuki Terasaka <yuki.terasaka@gmail.com>
2024-11-12 13:52:42 +0900

    Add 04 The HTTP Client Service

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,7 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+// use Symfony\Contracts\HttpClient\HttpClientInterface as HttpClientHttpClientInterface;
+
 use function Symfony\Component\String\u;
+// use Symfony\Contracts\HttpClientInterface;
 
 class VinylController extends AbstractController
 {
@@ -28,10 +32,13 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(string $slug = null): Response
+    public function browse(HttpClientInterface $httpClient, string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-        $mixes = $this->getMixes();
+        // $mixes = $this->getMixes();
+
+        $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+        $mixes = $response->toArray();
 
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,

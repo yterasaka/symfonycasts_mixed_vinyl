e0ea22b8bf53f6ef9df604d2e850766f4edf1143
Yuki Terasaka <yuki.terasaka@gmail.com>
2024-11-19 14:29:57 +0900

    Add 17 named autowiring

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\Attribute\Autowire;
 class MixRepository
 {
     public function __construct(
-        private HttpClientInterface $httpClient,
+        private HttpClientInterface $githubContentClient,
         private CacheInterface $cache,
         #[Autowire('%kernel.debug%')]
         private bool $isDebug
@@ -20,7 +20,7 @@ class MixRepository
     {
         return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
             $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
-            $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+            $response = $this->githubContentClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json');
             return $response->toArray();
         });
     }
